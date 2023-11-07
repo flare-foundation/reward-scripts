a00b69e872ceeb56c4b5096a9ba2a529ec6d8083
Aljaž Starc <aljaz@flare.network>
2023-11-07 12:10:03 +0100

    update

--- .github/workflows/get-current-reward-epoch.js
+++ .github/workflows/get-current-reward-epoch.js
@@ -4,4 +4,4 @@ const diffInMs = new Date() - startDate
 const diff = (diffInMs / 1000 / 60 / 60) / 84
 //           (      diff in hours      )    `- epoch increases every 84 hours
 const currentEpoch = Math.floor(diff)
-console.log(currentEpoch)
+console.log(currentEpoch - 1)
